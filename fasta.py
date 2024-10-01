69a1ac1d9ef92e3b19b430c34da49cf69c4fa646
Meenu <63814693+meenub-18@users.noreply.github.com>
2024-10-01 16:21:38 +0530

    Delete fasta.py

--- fasta.py
+++
@@ -1,8 +0,0 @@
-#parsing fasta file
-
-from Bio import SeqIO
-for sequence in SeqIO.parse('globin.fasta', "fasta"):
-    print(sequence.id)
-    print(repr(sequence.seq))
-    print(len(sequence))
-    
